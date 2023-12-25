1bd3a4a12b483821b947748ba6671a94c2afb7b2
SumJest <summedjesters@gmail.com>
2023-12-25 14:53:12 +0500

    translate to english

--- src/application/views.py
+++ src/application/views.py
@@ -7,14 +7,16 @@ from .app import app
 
 service = AnalyzeService()
 
+
 @app.get('/')
 def index():
     return render_template("index.html", functions=[func for func in ElasticFunction])
 
+
 @app.post('/')
 def process():
     if 'file' not in request.files or not request.files['file'].filename:
-        flash('Загрузите файл')
+        flash('Please upload file')
         return redirect(request.url)
     file = request.files['file']
     match request.form['action']:
@@ -22,18 +24,18 @@ def process():
             try:
                 return service.process_analyze(file.stream.read())
             except TensorNotFound as exception:
-                flash('Тензор не найден')
+                flash('Tensor not found')
         case '2':
             try:
-                return service.process_surface(file.stream.read(), ElasticFunction(int(request.form['function'])))
+                return service.process_surface(file.stream.read(), ElasticFunction.by_name(request.form['function']))
             except TensorNotFound as exception:
-                flash('Тензор не найден')
+                flash('Tensor not found')
         case '3':
             try:
-                return service.process_projections(file.stream.read(), ElasticFunction(int(request.form['function'])))
+                return service.process_projections(file.stream.read(), ElasticFunction.by_name(request.form['function']))
             except TensorNotFound as exception:
-                flash('Тензор не найден')
+                flash('Tensor not found')
         case _:
-            flash('Неверное действие')
+            flash('Wrong action')
             return redirect(request.url)
     return render_template("index.html", functions=[func for func in ElasticFunction])
